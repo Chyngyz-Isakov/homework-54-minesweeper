c1aecda83d0bc65341a3847be892c717ecc0c010
Chyngyz Isakov <ellegantus@gmail.com>
2023-05-27 13:02:51 +0600

    I created a function that will create an array of 36 objects of the following form in a loop: {hasItem: false, clicked: false}

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => {
 
-    </div>
-  );
+    const createItems = () => {
+        const items = [];
+        for (let i = 1; i <= 36; i++) {
+            items.push({hasItem: false, clicked: false});
+        }
+
+        const randomBox = Math.floor(Math.random() * items.length);
+        items[randomBox].hasItem = true;
+        return items;
+    }
+    console.log(createItems());
+
+
+    return (
+        <div className="App">
+            <h2>Try to find a ring!</h2>
+            <div className='box-wrap'>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
